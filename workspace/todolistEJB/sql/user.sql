2f54969e871098f83d4e05148394e93a9f273fea
Rodolfo Campos <camposer@gmail.com>
2014-10-24 18:52:21 +0200

    Modificaciones para autenticación JEE (realm jdbc)

--- workspace/todolistEJB/sql/user.sql
+++ workspace/todolistEJB/sql/user.sql
@@ -1,19 +1,21 @@
-DROP TABLE "user";
-DROP TABLE "group";
+DROP TABLE groups;
+DROP TABLE users;
 
-CREATE TABLE "user" (
+CREATE TABLE users (
 	username VARCHAR(50) NOT NULL,
-	passsword VARCHAR(100) NOT NULL,
-	groupname VARCHAR(50),
+	password VARCHAR(100) NOT NULL,
 	PRIMARY KEY(username)
 );
 
-CREATE TABLE "group" (
+CREATE TABLE groups (
+	username VARCHAR(50) NOT NULL,
 	groupname VARCHAR(50) NOT NULL,
-	PRIMARY KEY (groupname)
+	PRIMARY KEY (username, groupname),
+	FOREIGN KEY (username) REFERENCES users(username)
 );
 
-INSERT INTO "user" VALUES('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'ADMIN');
-INSERT INTO "group" VALUES('ADMIN');
-INSERT INTO "group" VALUES('USER');
+INSERT INTO users VALUES('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
+INSERT INTO groups VALUES('admin', 'ADMIN');
+INSERT INTO groups VALUES('admin', 'USER');
 
+-- user: admin, password: admin
